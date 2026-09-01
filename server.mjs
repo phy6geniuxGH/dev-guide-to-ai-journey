16744e37ab432f6aa0b2a0b32e2750a91a2e8030
Francis Santos Dela Cruz <francisprogrammerdc@gmail.com>
2026-09-01 16:29:55 +0800

    dev_guide_ai/fsdc-260901: Adding first files into my developer's guide to AI

---
+++ server.mjs
@@ -0,0 +1,22 @@
+import express, { response } from "express"
+import { Ollama } from "ollama"
+
+const app = express();
+
+const ollama = new Ollama();
+
+app.get('/', async(request, response) => {
+    response.type('text/plain');
+
+    const modelResponse = await ollama.generate({
+        model: 'gemma4:12b',
+        prompt: "What is the capital of the Philippines?"
+    });
+
+    response.send(modelResponse.response);
+
+});
+
+app.listen(8000, () => {
+    console.log(`Server is running on port 8000`)
+})
---
+++ stream_server.mjs
@@ -0,0 +1,33 @@
+import express from "express";
+import { Ollama } from "ollama";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const ollama = new Ollama();
+
+app.post('/', async (request, response) => {
+    response.type('text/plain');
+
+    const body = request.body;
+
+    const streamIterator = await ollama.generate({
+        model: 'llama3.2',
+        prompt: body.question,
+        stream: true
+    });
+
+    for await (const chunk of streamIterator) {
+        response.write(chunk.response);
+    }
+
+    response.end();
+
+});
+
+app.listen(8000, () => {
+    console.log(`Server is running on port 8000`);
+});
